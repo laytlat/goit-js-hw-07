25ebf3c5c52143fb79592a88b20c8dec66ac498c
Alexandr Hrynenko <layt.lat@ukr.net>
2023-02-25 18:18:15 +0200

    first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const galleryItem = galleryItems
+  .map(
+    image => `<div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>`
+  )
+  .join('');
+
+galleryEl.innerHTML = galleryItem;
+
+galleryEl.addEventListener('click', onClickOpenModal);
+
+function onClickOpenModal(evt) {
+  evt.preventDefault();
+  const instance = basicLightbox.create(`
+      <img src="${evt.target.dataset.source}" width="100%" height="100%">
+  `);
+
+  instance.show();
+
+  window.addEventListener('keydown', onEscapeCloseModal);
+  function onEscapeCloseModal(e) {
+    if (e.key === 'Escape') {
+      instance.close();
+      window.removeEventListener('keydown', onEscapeCloseModal);
+    }
+  }
+}
