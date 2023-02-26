0e1d883805dba1f3c4db6d0a8f9c28c056bedf6f
Alexandr Hrynenko <layt.lat@ukr.net>
2023-02-26 10:43:46 +0200

    second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,19 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const galleryItem = galleryItems
+  .map(
+    image => `<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`
+  )
+  .join('');
+
+galleryEl.innerHTML = galleryItem;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
